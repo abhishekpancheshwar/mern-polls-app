bd76c741c25f3c3fbaa6711d788ddc7b05af3ae1
[Abhishek Pancheshwar] <[abhishekpancheswar8@gmail.com]>
2025-09-19 13:11:06 +0530

    Final backend setup for deployment

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -9,7 +9,7 @@ export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem('token'));
   const [loading, setLoading] = useState(true);
 
-  axios.defaults.baseURL = 'http://localhost:5001/api';
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
   useEffect(() => {
     const storedToken = localStorage.getItem('token');
